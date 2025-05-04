d9c7bfcc866d9704c7e3532dd351907a16a65496
Johann Fouassier <johann.fouassier16@gmail.com>
2025-05-05 00:23:49 +0200

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: {
+            light: '#a8e6cf',
+            DEFAULT: '#00b894',
+            dark: '#006d5b',
+          },
+        },
+      },
+    },
+    plugins: [],
+  };
+  
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#84ce35',
+          DEFAULT: '#a9c91c',
+          dark: '#6e9c4e',
+        },
+        texte: '#89765e',
+      },
+    },
+  },
+  plugins: [],
+};
